527f6791eb465da4a2160487825dd8e47fc96705
Luiz Henrique <luizhenriqueyhwh@gmail.com>
2023-04-07 11:05:17 -0300

    1_modificada: base/views.py, 2_modificada: dj_ultima/urls.py, add: basae/templates/contato.html

--- base/views.py
+++ base/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def inicio(request):
    return render(request, 'inicio.html')
+
+def contato(request):
+   contexto = {
+      'telefone': '(16) 99291-1234',
+      'responsavel': 'Maria da Silva Pereira',
+      'email': 'pettop@email.com',
+   }
+   return render(request, 'contato.html', contexto)
--- dj_ultima/urls.py
+++ dj_ultima/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from base.views import inicio
+from base.views import inicio, contato
 
 urlpatterns = [
     path('', inicio),
+    path('contato/', contato),
     path('admin/', admin.site.urls),
 ]
