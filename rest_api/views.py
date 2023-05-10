91d69ae8964b78e2720a96f7e5623dc80db6b2e9
Luiz Henrique <luizhenriqueyhwh@gmail.com>
2023-05-10 19:37:06 -0300

    modificado:  rest_api/views.py

--- rest_api/views.py
+++ rest_api/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT'])
 def hello_world(request):
     if request.method == 'POST':
-        return Response({'message': f'Hello, {request.data.get ("name")}'})
+        return Response({'message': f'Hello, {request.data.get("name")}'})
     
-    return Response ({'Hello': 'World API'})
+    elif request.method == 'PUT':
+        return Response({'Dolars': 'R$2.000,000,45' })
+    
+    else:
+        return Response ({'Meu Saldo': 'R$5.110,200 To Rico $$$$$$!Ha!Ha!'})
