a892fd569865cf70bc1834c862911163f6151003
Isadora Garcia Ferrão <isadora-gf@hotmail.com>
2023-06-28 12:59:53 +0200

    Update urls.py

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
+from rest_api.views import hello_world, AgendamentoModelViewSet
 from rest_framework.routers import SimpleRouter
 
-from rest_api.serializers import AgendamentoModelViewSet,  hello_world
-
 app_name = 'rest_api'
-
 router = SimpleRouter(trailing_slash=False)
 router.register('agendamento', AgendamentoModelViewSet)
 
